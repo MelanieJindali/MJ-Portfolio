b71ab7d768ccc35ef99343471161315265ce0b9e
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-09-07 18:57:13 -0500

    Remove old toggle button functions

--- assets/index.js
+++ assets/index.js
@@ -1,29 +1,3 @@
-$(document).ready(function(e) {
-    $win = $(window);
-    $navbar = $('#header');
-    $toggle = $('.toggle-button');
-    var width = $navbar.width();
-    toggle_onclick($win,$navbar,width);
-
-    // Resize
-    $win.resize(function() {
-        toggle_onclick($win,$navbar,width);
-    });
-
-    $toggle.click(function(e) {
-        $navbar.toggleClass("toggle-left");
-    });
-
-});
-
-function toggle_onclick($win,$navbar,width) {
-    if ($win.width() <= 768) {
-        $navbar.css({left:`-${width}px`});
-    } else {
-        $navbar.css({left:'0px'})
-    }
-}
-
 var typed = new Typed('#typed', {
     strings: [
       'a Web Developer.',
@@ -32,14 +6,13 @@ var typed = new Typed('#typed', {
     typeSpeed: 50,
     backSpeed: 50,
     loop: true
-})
-
+});
 
 document.querySelectorAll('a[href^="#"').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView( {
-            behavior: 'smooth'
-        });
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
+  });
 });
